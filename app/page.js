4e5d38577c5ca7d186e880f561e024113accc530
Anvesh Ramelli <anveshramelli1999@gmail.com>
2025-05-06 22:52:41 +0530

    chore: created new Next.js project and set up shadcn-ui

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hirebot</h1>
+      <Button className="bg-red-500 m-2 w-24 text-3xl px-16 py-6 cursor-pointer">Test</Button>
+    </div>
+  );
+}
